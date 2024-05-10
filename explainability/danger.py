53c818f7e94e7567b5dafb2a17eac6262bc84f20
simonasnow <simona.frenda@gmail.com>
2024-05-10 11:27:19 +0200

    updated

--- explainability/danger.py
+++ explainability/danger.py
@@ -1,6 +1,5 @@
-from transformers import  AutoTokenizer
+from transformers import  AutoTokenizer, pipeline, AutoModelForSequenceClassification
 import torch
-from transformers import AutoModelForSequenceClassification
 from safetensors.torch import  load_file
 import io
 from peft import LoraConfig, get_peft_model
@@ -8,13 +7,11 @@ from peft import set_peft_model_state_dict
 import numpy as np
 from functools import lru_cache
 import json
+import shap
 
 model_name = {}
 model_name["it"] = 'm-polignano-uniba/bert_uncased_L-12_H-768_A-12_italian_alb3rt0'
 model_name["en"] = 'm-polignano-uniba/bert_uncased_L-12_H-768_A-12_italian_alb3rt0'
-model = {}
-model["it"] = AutoModelForSequenceClassification.from_pretrained(model_name["it"], num_labels=2)
-model["en"] = AutoModelForSequenceClassification.from_pretrained(model_name["en"], num_labels=2)
 tokenizer = {}
 tokenizer["it"] = AutoTokenizer.from_pretrained(model_name["it"])
 tokenizer["en"] = AutoTokenizer.from_pretrained(model_name["en"])
@@ -27,34 +24,22 @@ class Irony():
     def __init__(self):
 
         self.lora_model = {}
-        self.lora_model["it"] = self.__model_lora("it")
-        self.lora_model["en"] = self.__model_lora("en")
+        self.lora_model["it"] = 'aequa-tech/lora-irony'
+        self.lora_model["en"] = 'aequa-tech/lora-irony'
 
-    def __model_lora(self,language):
-        """
-        This method creates an instance of lora used in our fine-tuned model to reduce the parameters of Language Model (and weight of the final model).
-        """
-        config = LoraConfig(
-            r=16,
-            lora_alpha=32,
-            target_modules=["query", "value"],
-            lora_dropout=0.01,
-            bias="none",
-            task_type="classifier",
-            modules_to_save=["classifier"]
-        )
-
-        lora_model = get_peft_model(model[language], config)
-        return lora_model
-   
     @lru_cache(maxsize=32)
-    def my_tokenizer(self, language, text):
-        tokenized = tokenizer[language].encode_plus(text,return_tensors='pt',max_length=512)
-        feat = tokenized['input_ids']
-        attention = tokenized['attention_mask']
-        return feat, attention
+    def __my_pipeline(self, lora_model, model_name, tokenizer):
+        model = AutoModelForSequenceClassification.from_pretrained(lora_model,num_labels=2)
+        classifier = pipeline("text-classification", model=model_name, tokenizer=tokenizer, top_k=None)
+        return classifier
+    
+    def __max(self, data,label):
+        max = np.max(data[:,:,label].values)
+        index = np.where(data[:,:,label].values == max)[1][0]
+        word = np.take(data[:,:,label].data, index)
+        return word.strip(), max
         
-    def get_irony(self, language, title: str, content: str):
+    def get_explaination(self, language, title: str, content: str):
         """
         input:
             @param title: str: string containing the title of a news
@@ -62,37 +47,34 @@ class Irony():
         output:
             - dictionary of the prediction: the absolute value = 1/0, local_normalisation = logit coming from sigmoid function, global_normalisation = None
         """
-        cp = 'features/nlp/models/'+language+'/adapter_irony.safetensors'
-        print(cp)
-        full_state_dict = load_file(cp)
-
-        #adatta il modello generale con i pesi del task specifico
-        set_peft_model_state_dict(self.lora_model[language], full_state_dict)
-        self.lora_model[language].eval()
-        sigmoid = torch.nn.Sigmoid()
-        
         result={
             "description" : "Prediction of irony in the text. A score near to 1 indicates the presence of irony, while a negative score or a score near to 0 the absence of irony."
         }
-
+        
         features = {"title" : title, "content" : content}
 
         for key, value in features.items():
-            feat, attention = self.my_tokenizer(language,value)
-        
-            score = self.lora_model[language](input_ids=feat, attention_mask=attention)
-            # print(score)
-            score_sig = sigmoid(score['logits'].detach())
-            absolute = torch.argmax(score_sig).item()
-            # local = score['logits'][0,1].item() #non normalizzato e logit solo della classe positiva
-            local = score_sig[0,1].item() # normalizzato con sigmoid e logit solo della classe positiva
-            # print(score_sig, absolute, local)
-        
+            if key != 'title':
+                continue
+            print(value)
+            classifier = self.__my_pipeline(self.lora_model[language], model_name[language], tokenizer[language])
+            results = classifier(value,truncation=True)
+            print(results)
+
+            shap_model = shap.models.TransformersPipeline(classifier, rescale_to_logits=False)
+            explainer = shap.Explainer(shap_model)
+            shap_values = explainer([value])
+            # print(shap_values) 
+
+            word_p, weight_p = self.__max(shap_values, 'LABEL_1') #get span only for positive class
+            # word_n, weight_n = self.__max(shap_values, 'LABEL_0')
+            print(word_p, weight_p)
+            
             result[key]={
                         "values":
                         {
-                         "absolute": round(absolute,3),   
-                         "local_normalisation": round(local,3),
+                         "word": word_p,
+                         "local_normalisation": round(weight_p, 3),
                          "global_normalisation": None,
                         },
                         'descriptions': {
@@ -111,34 +93,22 @@ class Flame():
     def __init__(self):
 
         self.lora_model = {}
-        self.lora_model["it"] = self.__model_lora("it")
-        self.lora_model["en"] = self.__model_lora("en")
+        self.lora_model["it"] = 'aequa-tech/lora-flame'
+        self.lora_model["en"] = 'aequa-tech/lora-flame'
 
-    def __model_lora(self,language):
-        """
-        This method creates an instance of lora used in our fine-tuned model to reduce the parameters of Language Model (and weight of the final model).
-        """
-        config = LoraConfig(
-            r=16,
-            lora_alpha=32,
-            target_modules=["query", "value"],
-            lora_dropout=0.01,
-            bias="none",
-            task_type="classifier",
-            modules_to_save=["classifier"]
-        )
-
-        lora_model = get_peft_model(model[language], config)
-        return lora_model
-   
     @lru_cache(maxsize=32)
-    def my_tokenizer(self, language,text):
-        tokenized = tokenizer[language].encode_plus(text,return_tensors='pt',max_length=512)
-        feat = tokenized['input_ids']
-        attention = tokenized['attention_mask']
-        return feat, attention
+    def __my_pipeline(self, lora_model, model_name, tokenizer):
+        model = AutoModelForSequenceClassification.from_pretrained(lora_model,num_labels=2)
+        classifier = pipeline("text-classification", model=model_name, tokenizer=tokenizer, top_k=None)
+        return classifier
+    
+    def __max(self, data,label):
+        max = np.max(data[:,:,label].values)
+        index = np.where(data[:,:,label].values == max)[1][0]
+        word = np.take(data[:,:,label].data, index)
+        return word.strip(), max
         
-    def get_flame(self, language,title: str, content: str):
+    def get_explaination(self, language, title: str, content: str):
         """
         input:
             @param title: str: string containing the title of a news
@@ -146,14 +116,7 @@ class Flame():
         output:
             - dictionary of the prediction: the absolute value = 1/0, local_normalisation = logit coming from sigmoid function, global_normalisation = None
         """
-        cp = 'features/nlp/models/'+language+'/adapter_hs.safetensors'
-        full_state_dict = load_file(cp)
 
-        #adatta il modello generale con i pesi del task specifico
-        set_peft_model_state_dict(self.lora_model[language], full_state_dict)
-        self.lora_model[language].eval()
-        sigmoid = torch.nn.Sigmoid()
-        
         result={
             "description" : "Prediction of flaem in the text. A score near to 1 indicates the presence of irony, while a negative score or a score near to 0 the absence of irony."
         }
@@ -161,20 +124,27 @@ class Flame():
         features = {"title" : title, "content" : content}
 
         for key, value in features.items():
-            feat, attention = self.my_tokenizer(language,value)
-        
-            score = self.lora_model[language](input_ids=feat, attention_mask=attention)
-            # print(score)
-            score_sig = sigmoid(score['logits'].detach())
-            absolute = torch.argmax(score_sig).item()
-            local = score_sig[0,1].item() 
-            # print(score_sig, absolute, local)
-        
+            if key != 'title':
+                continue
+            print(value)
+            classifier = self.__my_pipeline(self.lora_model[language], model_name[language], tokenizer[language])
+            results = classifier(value,truncation=True)
+            print(results)
+
+            shap_model = shap.models.TransformersPipeline(classifier, rescale_to_logits=False)
+            explainer = shap.Explainer(shap_model)
+            shap_values = explainer([value])
+            # print(shap_values) 
+
+            word_p, weight_p = self.__max(shap_values, 'LABEL_1') #get span only for positive class
+            # word_n, weight_n = self.__max(shap_values, 'LABEL_0')
+            print(word_p, weight_p)
+            
             result[key]={
                         "values":
                         {
-                         "absolute": round(absolute,3),   
-                         "local_normalisation": round(local,3),
+                         "word": word_p,
+                         "local_normalisation": round(weight_p, 3),
                          "global_normalisation": None,
                         },
                         'descriptions': {
@@ -191,35 +161,24 @@ class Stereotype():
     The detection of this pragmatic phenomenon in the texts is obtained thanks to a fine-tuned model starting from Language Models available for Italian.
     """
     def __init__(self):
+
         self.lora_model = {}
-        self.lora_model["it"] = self.__model_lora("it")
-        self.lora_model["en"] = self.__model_lora("en")
+        self.lora_model["it"] = 'aequa-tech/lora-stereotype'
+        self.lora_model["en"] = 'aequa-tech/lora-stereotype'
 
-    def __model_lora(self,language):
-        """
-        This method creates an instance of lora used in our fine-tuned model to reduce the parameters of Language Model (and weight of the final model).
-        """
-        config = LoraConfig(
-            r=16,
-            lora_alpha=32,
-            target_modules=["query", "value"],
-            lora_dropout=0.01,
-            bias="none",
-            task_type="classifier",
-            modules_to_save=["classifier"]
-        )
-
-        lora_model = get_peft_model(model[language], config)
-        return lora_model
-   
     @lru_cache(maxsize=32)
-    def my_tokenizer(self, language, text):
-        tokenized = tokenizer[language].encode_plus(text,return_tensors='pt',max_length=512)
-        feat = tokenized['input_ids']
-        attention = tokenized['attention_mask']
-        return feat, attention
+    def __my_pipeline(self, lora_model, model_name, tokenizer):
+        model = AutoModelForSequenceClassification.from_pretrained(lora_model,num_labels=2)
+        classifier = pipeline("text-classification", model=model_name, tokenizer=tokenizer, top_k=None)
+        return classifier
+    
+    def __max(self, data,label):
+        max = np.max(data[:,:,label].values)
+        index = np.where(data[:,:,label].values == max)[1][0]
+        word = np.take(data[:,:,label].data, index)
+        return word.strip(), max
         
-    def get_stereotype(self, language, title: str, content: str):
+    def get_explaination(self, language, title: str, content: str):
         """
         input:
             @param title: str: string containing the title of a news
@@ -227,14 +186,7 @@ class Stereotype():
         output:
             - dictionary of the prediction: the absolute value = 1/0, local_normalisation = logit coming from sigmoid function, global_normalisation = None
         """
-        cp = 'features/nlp/models/'+language+'/adapter_stereotype.safetensors'
-        full_state_dict = load_file(cp)
 
-        #adatta il modello generale con i pesi del task specifico
-        set_peft_model_state_dict(self.lora_model[language], full_state_dict)
-        self.lora_model[language].eval()
-        sigmoid = torch.nn.Sigmoid()
-        
         result={
             "description" : "Prediction of stereotypes in the text. A score near to 1 indicates the presence of irony, while a negative score or a score near to 0 the absence of irony."
         }
@@ -242,20 +194,27 @@ class Stereotype():
         features = {"title" : title, "content" : content}
 
         for key, value in features.items():
-            feat, attention = self.my_tokenizer(language,value)
-        
-            score = self.lora_model[language](input_ids=feat, attention_mask=attention)
-            # print(score)
-            score_sig = sigmoid(score['logits'].detach())
-            absolute = torch.argmax(score_sig).item()
-            local = score_sig[0,1].item()
-            # print(score_sig, absolute, local)
-        
+            if key != 'title':
+                continue
+            print(value)
+            classifier = self.__my_pipeline(self.lora_model[language], model_name[language], tokenizer[language])
+            results = classifier(value,truncation=True)
+            print(results)
+
+            shap_model = shap.models.TransformersPipeline(classifier, rescale_to_logits=False)
+            explainer = shap.Explainer(shap_model)
+            shap_values = explainer([value])
+            # print(shap_values) 
+
+            word_p, weight_p = self.__max(shap_values, 'LABEL_1') #get span only for positive class
+            # word_n, weight_n = self.__max(shap_values, 'LABEL_0')
+            print(word_p, weight_p)
+            
             result[key]={
                         "values":
                         {
-                         "absolute": round(absolute,3),   
-                         "local_normalisation": round(local,3),
+                         "word": word_p,
+                         "local_normalisation": round(weight_p, 3),
                          "global_normalisation": None,
                         },
                         'descriptions': {
@@ -273,16 +232,16 @@ if __name__ == '__main__':
 
     print("IRONY")
     irony = Irony()
-    print(json.dumps(irony.get_irony("it",title, content), indent=4))
-    print(json.dumps(irony.get_irony("en",title, content), indent=4))
+    print(json.dumps(irony.get_explaination("it",title, content), indent=4))
+    # print(json.dumps(irony.get_explaination("en",title, content), indent=4))
 
     print("FLAME")
     flame = Flame()
-    print(json.dumps(flame.get_flame("it",title, content), indent=4))
-    print(json.dumps(flame.get_flame("en",title, content), indent=4))
+    print(json.dumps(flame.get_explaination("it",title, content), indent=4))
+    # print(json.dumps(flame.get_flame("en",title, content), indent=4))
 
     print("STEREOTYPE")
     stereotypes = Stereotype()
-    print(json.dumps(stereotypes.get_stereotype("it",title, content), indent=4))
-    print(json.dumps(stereotypes.get_stereotype("en",title, content), indent=4))
+    print(json.dumps(stereotypes.get_explaination("it",title, content), indent=4))
+    # print(json.dumps(stereotypes.get_stereotype("en",title, content), indent=4))
 
